be236f8573013494929ae1a55cbbcf3cfab359ed
Kayxue <s0990006@gmail.com>
2026-07-02 13:39:05 +0800

    Update traits and structs

--- src/manipulators/mod.rs
+++ src/manipulators/mod.rs
@@ -1,18 +1,26 @@
-use image::DynamicImage;
-use std::io::Error;
+use anyhow::{Context, Result};
+use image::{DynamicImage, ImageReader};
 
 pub trait Reader {
-    fn read_images(&self, path: Vec<String>) -> Result<Vec<DynamicImage>, Error> {
-        todo!()
+    fn read_image(&self, path: String) -> Result<DynamicImage> {
+        ImageReader::open(path)?
+            .decode()
+            .with_context(|| "Failed to read image")
     }
 
-    fn write_images(&self, images: Vec<DynamicImage>) -> Result<(), Error> {
-        todo!()
+    fn write_image(&self, image: DynamicImage, path: String) -> Result<()> {
+        image.save(path).with_context(|| "Failed to write image")
     }
 }
 
 pub trait Manipulator {
-    fn manipulate_images(&self) -> Result<(), Error>;
+    fn manipulate_images(&self) -> Result<ProcessedResult>;
 }
 
-pub struct ImageManipulator<T: Reader + Manipulator>(T);
+pub struct ImageManipulator<T: Manipulator>(T);
+
+pub struct ProcessedResult {
+    pub total: u32,
+    pub success: u32,
+    pub errors: u32,
+}
