829ace8b40489c3dc30a8ecf56bc90c6698e8c52
Kayxue <s0990006@gmail.com>
2026-07-02 12:12:20 +0800

    Argument parsing

---
+++ README.md
@@ -0,0 +1,19 @@
+# Nautilus Image Converter Reborn (App)
+
+Application part of Nautilus Image Converter Reborn, which can help you convert, resize, and rotate images.
+
+> [!NOTE]
+> Still under heavy development. Stay tuned.
+
+## Roadmap
+- [X] Arguments receiving logic.
+- [ ] Read image
+- [ ] Save image
+- [ ] Basic image rotation using image
+- [ ] Basic Image resizing using fast_image_resize
+- [ ] Basic image resizing fallback using image
+- [ ] Top window detection
+- [ ] GUI
+- [ ] Image conversion
+- [ ] (if possible) Rewrite extension part using Rust.
+- [ ] (if possible) Merge nautilus-extension part into this repo.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use std::path::PathBuf;
+
+use argh::{FromArgValue, FromArgs};
+
+#[derive(FromArgValue, Debug)]
+enum Mode {
+    Resize,
+    Rotate,
+    Convert,
+}
+
+#[derive(FromArgs)]
+/// A image tool that can resize, rotate, and convert images.
+struct Args {
+    /// operation mode: resize, rotate, or convert.
+    #[argh(option, short = 'm')]
+    mode: Option<Mode>,
+    /// path to the input image file.
+    #[argh(option, short = 'p')]
+    path: Option<String>,
+    /// path to the output image file.
+    #[argh(option, short = 'o')]
+    output: Option<PathBuf>,
+}
+
 fn main() {
+    let Args { mode, path, output } = argh::from_env();
+    if mode.is_none() || path.is_none() || output.is_none() {
+        eprintln!("Error: mode, path, and output are required.");
+        std::process::exit(1);
+    }
+
+    let paths: Vec<PathBuf> = path
+        .unwrap()
+        .split(',')
+        .map(|e| e.parse().unwrap())
+        .collect::<Vec<_>>();
+
+    if paths.iter().any(|e| !e.exists()) {
+        eprintln!("Error: one or more input paths do not exist.");
+        std::process::exit(1);
+    }
+
+    if !output.as_ref().unwrap().exists() {
+        eprintln!("Error: output path does not exist.");
+        std::process::exit(1);
+    }
+
+    if !output.as_ref().unwrap().is_dir() {
+        eprintln!("Error: output path is not a directory.");
+        std::process::exit(1);
+    }
+
     println!("Hello, world!");
 }
