11f85f38584f8d573b8bcbd99ec9303cf9463698
Kayxue <s0990006@gmail.com>
2026-07-14 23:32:43 +0800

    Creating Resize page

--- src/window.rs
+++ src/window.rs
@@ -1,10 +1,11 @@
 use gtk::{
-    Button, HeaderBar, Window,
-    prelude::{ButtonExt, GtkWindowExt},
+    Adjustment, Align, Box, Button, CheckButton, ComboBoxText, DropDown, HeaderBar, Label,
+    Orientation, SpinButton, StringList, Window,
+    prelude::{BoxExt, ButtonExt, CheckButtonExt, GtkWindowExt, OrientableExt, WidgetExt},
 };
 use relm4::{
-    Component, ComponentController, ComponentParts, ComponentSender, Controller, SimpleComponent,
-    component,
+    Component, ComponentController, ComponentParts, ComponentSender, Controller, RelmWidgetExt,
+    SimpleComponent, component,
 };
 
 use crate::{
@@ -86,23 +87,129 @@ impl SimpleComponent for HeaderModel {
 }
 
 pub struct ResizeBodyModel {
-
+    pub cur_percent: u8,
+    pub cur_width: u16,
+    pub cur_height: u16,
 }
 
-pub enum ResizeBodyOutput{
+#[derive(Debug)]
+pub enum ResizeBodyOutput {
     UpdateImageSize(ResizeKind),
     UpdateOutputMode(OutputMode),
 }
 
-pub struct RotateBodyModel {
+#[component(pub)]
+impl SimpleComponent for ResizeBodyModel {
+    type Init = ();
 
+    type Input = ();
+    type Output = ResizeBodyOutput;
+
+    view! {
+        Box {
+            set_hexpand: true,
+            set_orientation: Orientation::Vertical,
+            set_margin_all: 12,
+            set_spacing: 12,
+
+            Label {
+                set_valign: Align::Center,
+                set_xalign: 0f32,
+                set_label: "&lt;b&gt;Image Size&lt;/b&gt;",
+                set_use_markup: true
+            },
+
+            Box {
+                set_valign: Align::Center,
+                set_spacing: 12,
+
+                Label {
+                    set_halign: Align::Center
+                },
+
+                Box {
+                    set_hexpand: true,
+                    set_orientation: Orientation::Vertical,
+                    set_spacing: 6,
+
+                    Box {
+                        set_valign: Align::Center,
+                        set_spacing: 6,
+
+                        #[name(default_size_radiobutton)]
+                        CheckButton {
+                            set_halign: Align::Center,
+                            set_label: Some("Select a size"),
+                            set_use_underline: true
+                        },
+
+                        DropDown {
+                            set_hexpand: true,
+                            set_model: Some(&StringList::new(&["96x96", "128x128", "640x640", "800x800", "1024x768", "1280x960"]))
+                        },
+
+                        Label {
+                            set_halign: Align::Center,
+                            set_label: "pixels"
+                        }
+                    }
+                },
+
+                Box {
+                    set_vexpand: true,
+                    set_spacing: 6,
+
+                    #[name(custom_pct_radio_button)]
+                    CheckButton {
+                        set_halign: Align::Center,
+                        set_label: Some("Scale:"),
+                        set_use_underline: true,
+                        set_group: Some(&default_size_radiobutton),
+                        set_active: true
+                    },
+
+                    SpinButton {
+                        set_hexpand: true,
+                        set_adjustment: &Adjustment::new(50f64, 1f64, 100f64, 1f64, 10f64, 0f64)
+                    },
+
+                    Label {
+                        set_halign: Align::Center,
+                        set_label: "percent"
+                    }
+                },
+
+                #[name(hbox5)]
+                Box {
+
+                }
+            }
+        }
+    }
+
+    fn init(
+        _init: Self::Init,
+        root: Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let model = ResizeBodyModel {
+            cur_percent: 50,
+            cur_width: 1000,
+            cur_height: 1000,
+        };
+
+        let widgets = view_output!();
+
+        ComponentParts { model, widgets }
+    }
 }
 
-pub enum RotateBodyOutput{
+pub struct RotateBodyModel {}
+
+pub enum RotateBodyOutput {
     UpdateAngle(RotationAngle),
     UpdateOutputMode(OutputMode),
 }
-
 
 pub struct AppModel {
     general_config: GeneralConfig,
